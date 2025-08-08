39ef7eaa8ff2ea2a008d05213020bd4f7155eebe
nostalgia296 <3570965752@qq.com>
2025-08-08 22:47:29 +0800

    交互

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/nostalgia296/asar-go/asar"
+)
+
+func main() {
+	args := os.Args
+
+	// 检查参数数量是否足够
+	if len(args) < 2 {
+		fmt.Println("Usage: <command> <source> <destination>")
+		fmt.Println("Commands:")
+		fmt.Println("  p - Pack a directory into an ASAR file")
+		fmt.Println("  e - Extract an ASAR file into a directory")
+		return
+	}
+
+	// 检查命令
+	switch args[1] {
+	case "p":
+		if len(args) < 4 {
+			fmt.Println("Usage: p <source directory> <output ASAR file>")
+			return
+		}
+		// 调用 Pack 方法
+		asar.Pack(args[2], args[3])
+	case "e":
+		if len(args) < 4 {
+			fmt.Println("Usage: e <ASAR file> <output directory>")
+			return
+		}
+		// 调用 Unpack 方法
+		asar.Unpack(args[2], args[3])
+	default:
+		fmt.Println("Invalid command. Use 'p' to pack or 'e' to extract.")
+	}
+}
