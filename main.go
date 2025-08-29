8fc97a421cc80b8631b8aca8cc816650e32ffbdf
nostalgia296 <3570965752@qq.com>
2025-08-29 11:38:10 +0800

    实现l参数

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"encoding/json"
 	"github.com/nostalgia296/asar-go/asar"
 )
 
@@ -11,7 +12,10 @@ var (
     GoVersion string
     BuildAt string
     Author = "Nostalgia"
+    jsonMap map[string]interface{}
 )
+
+
 func main() {
 	args := os.Args
 
@@ -39,8 +43,18 @@ func main() {
 			fmt.Println("Usage: e <ASAR file> <output directory>")
 			return
 		}
-		// 调用 Unpack 方法
-		asar.Unpack(args[2], args[3])
+	case "l":
+	    if len(args) < 3 {
+			fmt.Println("Usage: l <ASAR file> ")
+			return
+			}
+        jsons, _ := asar.ReadJson(args[2])
+	    err := json.Unmarshal([]byte(jsons), &jsonMap)
+	    if err != nil {
+		    fmt.Println("Error unmarshalling JSON")
+	                 }
+	    asar.Traverse(jsonMap["files"].(map[string]interface{}), "./")
+
     case "v":
         fmt.Printf("Version : %s \n", Version)
         fmt.Printf("BuildAt : %s \n", BuildAt)
